cb0931352994037c901da6769e0c4cd3c1d2d33a
CuongkeisMe <nqc23082005@gmail.com>
2025-03-27 22:20:50 +0700

    Initial commit

---
+++ src/main/java/com/example/projectagile/ProjectAgileApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAgileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAgileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectagile/ProjectAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
